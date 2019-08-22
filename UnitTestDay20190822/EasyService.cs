82e9048ae3bbc3fcae99d9f7a6509aa719376d21
TenjoViolet@gmail.com <TenjoViolet@gmail.com>
2019-08-22 11:39:04 +0800

    加入專案檔案。

---
+++ UnitTestDay20190822/EasyService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestDay20190822
+{
+    public class EasyService
+    {
+        private int _intBaseNum = 2;
+
+        public  int GetCalResult()
+        {
+            return _intBaseNum + 5;
+        }
+
+        public int GetCalResult2()
+        {
+            return _intBaseNum + 8;
+        }
+    }
+}
---
+++ UnitTestDay20190822Tests/EasyServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestDay20190822;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestDay20190822.Tests
+{
+    [TestClass()]
+    public class EasyServiceTests
+    {
+        [TestMethod()]
+        public void GetCalResultTest()
+        {
+            //arrange
+            var esSut = new EasyService();
+            var expected = 7;
+
+            //act
+            var actual = esSut.GetCalResult();
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void GetCalResult2Test()
+        {
+            //arrange
+            var esSut = new EasyService();
+            var expected = 10;
+
+            //act
+            var actual = esSut.GetCalResult2();
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
